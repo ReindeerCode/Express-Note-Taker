143b3a7a16fcbca414892b1cb8c9276823facf8f
MJRudolph <ReindeerCode@gmail.com>
2020-12-08 14:12:23 -0500

    Server.js file added

---
+++ server.js
@@ -0,0 +1,11 @@
+// Dependencies
+// ===========================================================
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
