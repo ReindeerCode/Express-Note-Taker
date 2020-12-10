d96a568b58cd3d10c86748af8efb20b6c672cb59
MJRudolph <ReindeerCode@gmail.com>
2020-12-10 14:00:51 -0500

    Notes Path Created

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-// Dependencies
-// ===========================================================
+// Dependencies ===============================================
 const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
-
+const PORT = 3050;
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+// end Dependencies ============================================
+
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "./develop/public/notes.html"));
+});
+
+// listen
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
+// end listen
